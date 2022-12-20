8440fd55fc916ef69a967c154e9a0a6fac596a98
Ben Botsford <ben.botsford@flatironschool.com>
2022-12-20 12:07:29 -0600

    finish tests

--- lib/regex.py
+++ lib/regex.py
@@ -1,10 +1,13 @@
 import re
 
-name = r""
+# NOTE: There are only a few tests included, so multiple solutions will work.
+# Feel free to encourage students to find oversights and add tests to this lab!
+
+name = r"[A-Z][']*([A-z][ \-']{0,1})+"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"\({0,1}[\d]{3}\){0,1}[- ]{0,1}[\d]{3}-{0,1}[\d]{4}"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"[A-z]([A-z0-9][\.]{0,1})+[A-z0-9]+@[A-z0-9]+\.[a-z]{2,}"
 email_regex = re.compile(email_address)
--- lib/testing/regex_test.py
+++ lib/testing/regex_test.py
@@ -5,20 +5,89 @@ from regex import name_regex, phone_regex, email_regex
 class TestNameRegEx:
     '''name_regex in regex.py'''
 
-    def test_matches_its_such_a_lovely_day(self):
-        '''matches the string "It's such a lovely day today."'''
-        assert(name_regex.fullmatch("It's such a lovely day today."))
+    def test_matches_john_cena(self):
+        '''matches the string "John Cena".'''
+        assert name_regex.fullmatch("John Cena")
+
+    def test_matches_anya_taylor_joy(self):
+        '''matches the string "Anya Taylor-Joy".'''
+        assert name_regex.fullmatch("Anya Taylor-Joy")
+
+    def test_matches_dangelo(self):
+        '''matches the string "D'Angelo".'''
+        assert name_regex.fullmatch("D'Angelo")
+
+    def test_does_not_match_empty(self):
+        '''does not match an empty string.'''
+        assert not name_regex.fullmatch("")
+
+    def test_does_not_match_lowercase_john_cena(self):
+        '''does not match a lowercase string.'''
+        assert not name_regex.fullmatch("john cena")
+
+    def test_does_not_match_string_with_numbers(self):
+        '''does not match a string containing numbers.'''
+        assert not name_regex.fullmatch("J0hn C3na")
+
+    def test_does_not_match_string_with_whitespace_other_than_one_space(self):
+        '''does not match a string containing non-single-space whitespace sequences.'''
+        assert not name_regex.fullmatch("John   Cena")
 
 class TestPhoneRegEx:
     '''phone_regex in regex.py'''
 
-    def test_matches_its_such_a_lovely_day(self):
-        '''matches the string "It's such a lovely day today."'''
-        assert(phone_regex.fullmatch("It's such a lovely day today."))
+    def test_matches_format_nnnnnnnnnn(self):
+        '''matches the string "5555555555"'''
+        assert phone_regex.fullmatch("5555555555")
+        
+    def test_matches_format_nnn_nnn_nnnn(self):
+        '''matches the string "555-555-5555"'''
+        assert phone_regex.fullmatch("555-555-5555")
+
+    def test_matches_format_parentheses_space(self):
+        '''matches the string "(555) 555-5555'''
+        assert phone_regex.fullmatch("(555) 555-5555")
+
+    def test_does_not_match_9_digits(self):
+        '''does not match the string "555555555"'''
+        assert not phone_regex.fullmatch("555555555")
+    
+    def test_does_not_match_non_digit_characters(self):
+        '''does not match the string "aaaaaaaaaa"'''
+        assert not phone_regex.fullmatch("aaaaaaaaaa")
+
+    def test_does_not_match_multiple_dashes(self):
+        '''does not match the string "555--555--5555".'''
+        assert not phone_regex.fullmatch("555--555--5555")
 
 class TestEmailRegEx:
     '''email_regex in regex.py'''
 
-    def test_matches_its_such_a_lovely_day(self):
-        '''matches the string "It's such a lovely day today."'''
-        assert(email_regex.fullmatch("It's such a lovely day today."))
+    def test_matches_name_email(self):
+        '''matches an email address with all alpha characters, @, and a domain.'''
+        assert email_regex.fullmatch("johncena@wwe.com")
+
+    def test_matches_dot_name_email(self):
+        '''matches an email address with alpha characters, single dots, @, and a domain.'''
+        assert email_regex.fullmatch("john.cena@wwe.com")
+
+    def test_matches_dot_name_number_email(self):
+        '''matches an email address with alphanumeric characters, single dots, @, and a domain.'''
+        assert email_regex.fullmatch("john.cena123@wwe.com")
+
+    def test_does_not_match_first_character_non_alpha_email(self):
+        '''does not match an email address that begins with a number.'''
+        assert not email_regex.fullmatch("123john.cena@wwe.com")
+
+    def test_does_not_match_no_at_email(self):
+        '''does not match an email address without an @.'''
+        assert not email_regex.fullmatch("johncena.com")
+
+    def test_does_not_match_invalid_character_email(self):
+        '''does not match an email address with a $.'''
+        assert not email_regex.fullmatch("john$cena@wwe.com")
+    
+    def test_does_not_match_no_domain_email(self):
+        '''does not match an email address without a domain.'''
+        assert not email_regex.fullmatch("johncena@")
+        
